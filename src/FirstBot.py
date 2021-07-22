6ece496628d0c85d6689a87789bbbf7a48490887
AndreyGTHB <andr.g.tuk@gmail.com>
2021-07-22 17:20:39 +0700

    Some changes

--- src/FirstBot.py
+++ src/FirstBot.py
@@ -6,6 +6,8 @@ from sc2.constants import NEXUS, PROBE, PYLON, ASSIMILATOR, GATEWAY, \
 
 
 class FirstBot(sc2.BotAI):
+    PROBES_NEEDED = 60
+
     async def on_step(self, iteration):
         await self.distribute_workers()
         await self.build_workers()
@@ -17,12 +19,9 @@ class FirstBot(sc2.BotAI):
         await self.build_army_structures()
         await self.build_army()
 
-    def probes_need(self) -> int:
-        return self.units(NEXUS).amount * 22
-
     async def build_workers(self):
         for nexus in self.units(NEXUS).ready.idle:
-            if self.units(PROBE).amount < self.probes_need():
+            if self.units(PROBE).amount < self.PROBES_NEEDED:
                 if not self.can_afford(PROBE):
                     return
                 await self.do(nexus.train(PROBE))
@@ -34,28 +33,30 @@ class FirstBot(sc2.BotAI):
 
     async def expand(self):
         nexuses = self.units(NEXUS)
-        if nexuses.amount < 2 or self.units(PROBE).amount >= self.probes_need() - 4:
+        if nexuses.amount < 2 or self.units(PROBE).amount >= self.units(NEXUS) * 22 - 4:
             if self.can_afford(NEXUS):
                 await self.expand_now()
 
     async def build_assimilators(self):
+        if self.units(GATEWAY).empty:
+            return
         for nexus in self.units(NEXUS).ready:
-            geysers = self.state.vespene_geyser.closer_than(10.0, nexus)
+            geysers = self.state.vespene_geyser.closer_than(10, nexus)
             for geyser in geysers:
                 if not self.can_afford(ASSIMILATOR):
-                    break
+                    return
                 worker = self.select_build_worker(geyser.position)
                 if worker is None:
                     break
-                if not self.units(ASSIMILATOR).closer_than(1.0, geyser).exists:
+                if not self.units(ASSIMILATOR).closer_than(1, geyser).exists:
                     await self.do(worker.build(ASSIMILATOR, geyser))
 
     async def build_army_structures(self):
         pylons = self.units(PYLON).ready
         if pylons.exists:
             pylon = pylons.random
-            if not self.units(GATEWAY).exists:
-                if self.can_afford(GATEWAY):
+            if not self.units(GATEWAY).ready.exists:
+                if self.can_afford(GATEWAY) and not self.already_pending(GATEWAY):
                     await self.build(GATEWAY, pylon)
             else:
                 if not self.units(CYBERNETICSCORE).exists:
@@ -67,6 +68,9 @@ class FirstBot(sc2.BotAI):
             if not self.can_afford(STALKER):
                 return
             await self.do(gate.train(STALKER))
+
+    async def control_army(self):
+        stalkers = self.units(STALKER)
 
 
 run_game(
